877170c3b3ef9b11bf874b22a6aa0d4fe4d9ede8
yara30999 <yaranaserelniny@gmail.com>
2025-02-28 19:11:30 +0200

    connect the project to firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
