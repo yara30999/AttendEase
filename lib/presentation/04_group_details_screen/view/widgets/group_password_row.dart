c1e59530842c99aa1e1ad86b14c34b52cd9049e9
yara30999 <yaranaserelniny@gmail.com>
2025-03-13 10:31:19 +0200

    delete useless code.

--- lib/presentation/04_group_details_screen/view/widgets/group_password_row.dart
+++ lib/presentation/04_group_details_screen/view/widgets/group_password_row.dart
@@ -1,11 +1,9 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../app/functions.dart';
 import '../../../resourses/colors_manager.dart';
 import '../../../resourses/styles_manager.dart';
-import '../../view_model/group_details_bloc/group_details_bloc.dart';
 
 class GroupPasswordRow extends StatelessWidget {
   final String passwordTxt;
@@ -29,10 +27,4 @@ class GroupPasswordRow extends StatelessWidget {
       ],
     );
   }
-}
-
-String getPassword(BuildContext context) {
-  final _groupInfo = context.watch<GroupDetailsBloc>().groupInfo;
-  final _password = _groupInfo?.password ?? "";
-  return _password;
 }
